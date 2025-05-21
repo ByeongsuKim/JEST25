db4afba97a85dd117955644476643d7f9366ebb9
초등인사김병수\user <code.ssu@gmail.com>
2025-05-21 19:04:03 +0900

    navigation 제목 없애기

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import { useNavigation } from 'expo-router';
+import { useEffect } from 'react';
 import { Dimensions, StatusBar, StyleSheet, Text, View } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 const boxWidth = width / 3;
 const boxHeight = height / 3;
 
-const App = () => {
+export default function Home() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
-      {/* 상태 표시줄 숨기기 */}
-      <StatusBar hidden />
+      <StatusBar hidden /> {/* 상태 표시줄 숨기기 */}
 
       {Array.from({ length: 9 }, (_, index) => (
         <View key={index} style={[styles.box, { width: boxWidth, height: boxHeight }]}>
@@ -18,7 +24,7 @@ const App = () => {
       ))}
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -37,5 +43,3 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
-
-export default App;
